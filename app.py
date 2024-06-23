f918441f75c95195f25eb1c8ec83bef2c17cc9b7
Rusty-Pea <27276372+Rusty-Pea@users.noreply.github.com>
2024-06-23 15:30:56 +0100

    Adding secrets

--- app.py
+++ app.py
@@ -2,6 +2,37 @@ import streamlit as st
 import pandas as pd
 from rag_setup import initialize_rag, retrieve_relevant_entries
 from openai_integration import process_query
+import hmac
+
+# *** PASSWORD CHECK ***
+def check_password():
+    """Returns `True` if the user had the correct password."""
+
+    def password_entered():
+        """Checks whether a password entered by the user is correct."""
+        if hmac.compare_digest(st.session_state["password"], st.secrets["password"]):
+            st.session_state["password_correct"] = True
+            del st.session_state["password"]  # Don't store the password.
+        else:
+            st.session_state["password_correct"] = False
+
+    # Return True if the password is validated.
+    if st.session_state.get("password_correct", False):
+        return True
+
+    # Show input for password.
+    st.text_input(
+        "Password", type="password", on_change=password_entered, key="password"
+    )
+    if "password_correct" in st.session_state:
+        st.error("😕 Password incorrect")
+    return False
+
+
+if not check_password():
+    st.stop()  # Do not continue if check_password is not True.
+
+# *** MAIN APP STARTS HERE ***
 
 # Initialize RAG system
 @st.cache_resource
@@ -28,6 +59,10 @@ threshold = st.sidebar.slider(
     value=0.6,  # default value
     step=0.05,
     help="Adjust this to control how closely fellows need to match your query. Higher values mean stricter matching."
+)
+
+st.sidebar.info(
+    "Do you want something added or changed here? Message Rus"
 )
 
 # MAIN BIT
